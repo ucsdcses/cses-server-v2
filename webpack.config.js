b7c46a5161acaa7e08ded920df485207509ecd17
Richard Torres <richardtorres314@gmail.com>
2018-06-05 13:49:53 -0400

    Starter Commit to setup packages and frameworks
    Update README.md (#1)
    
    Updating `wriiten` to to `written`.
    
    add babel-plugin-transform-class-properties (#4)
    
    adding .travis.yml
    
    Update dependencies to enable Greenkeeper 🌴 (#7)
    
    * chore(package): update dependencies
    
    * docs(readme): add Greenkeeper badge
    
    Update README.md
    
    adding image loaders
    
    example to include image
    
    readme update
    
    chore(package): update css-loader to version 1.0.0 (#8)
    
    chore(package): update style-loader to version 0.22.0 (#12)
    
    chore(package): update style-loader to version 0.23.0 (#14)
    
    chore(package): update concurrently to version 4.0.0 (#13)
    
    regen yarn.lock
    
    removing yarn.lock
    
    upgrade to babel 7
    
    babel upgrade fixed
    
    build before run production server (#24)
    
    chore(package): update babel-eslint to version 10.0.0 (#22)
    
    chore(package): update file-loader to version 2.0.0 (#26)
    
    Use doublequotes for Windows (#27)
    
    Switching to escaped doublequotes for concurrently makes it work on Windows platform.
    See concurrently introduction https://github.com/kimmobrunfeldt/concurrently#readme
    
    async/await support
    
    readme update
    
    chore(package): update file-loader to version 3.0.0 (#35)
    
    chore(package): update css-loader to version 2.0.0 (#33)
    
    chore(package): update clean-webpack-plugin to version 1.0.0 (#29)
    
    Update index.js (#37)
    
    * Update index.js
    
    Fixed errors on port binding.
    
    * Update src/server/index.js
    
    Co-Authored-By: ernestpascual <eepascual@gmail.com>
    
    Add Support to JSX Files (#39)
    
    starter code setup

--- webpack.config.js
+++ webpack.config.js
@@ -5,15 +5,14 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 const outputDirectory = 'dist';
 
 module.exports = {
-  entry: './src/client/index.js',
+  entry: ['babel-polyfill', './src/client/index.js'],
   output: {
     path: path.join(__dirname, outputDirectory),
     filename: 'bundle.js'
   },
   module: {
-    rules: [
-      {
-        test: /\.js$/,
+    rules: [{
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader'
@@ -22,8 +21,15 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        loader: 'url-loader?limit=100000'
       }
     ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
   },
   devServer: {
     port: 3000,
