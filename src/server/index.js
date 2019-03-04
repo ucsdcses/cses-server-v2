199219776c9a4253f79bf630455984e64bccc08d
Saurabh Kanhegaonkar <saurabh.kanhegaonkar@gmail.com>
2019-03-04 15:12:56 -0800

    bring in sync with prev repo

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,18 @@
 const express = require('express');
-const os = require('os');
+const logger = require('./logger');
 
 const app = express();
 
-app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+// Initialiaze API routes
 
-app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
+/* /: default route  */
+app.get('/', (req, res) => {
+  res.send({ cses: 'Website will be sicc' });
+});
+
+/* /api/board: Get the details of the board members from a .json file stored in data/ folder and send it as a response */
+
+
+// Serve the app on a localhost port
+logger.info('Server started at port 5000');
+app.listen(5000);
